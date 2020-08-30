34a4bf109c606f66c6a19df81fbe712a3ddbf688
shuklaamar <amarshukla636@gmail.com>
2020-08-30 11:58:14 +0530

    Updates

---
+++ precache-manifest.cba28ffc7262185c7e355a6917e3cbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bbf1b2824feab2e768df66eb28e050",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "090fe818dc21d1389469",
+    "url": "/portfolio/static/css/main.2b23d8e3.chunk.css"
+  },
+  {
+    "revision": "09793f341f0db705cbf2",
+    "url": "/portfolio/static/js/2.897b9f51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.897b9f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090fe818dc21d1389469",
+    "url": "/portfolio/static/js/main.b8fbe95f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
